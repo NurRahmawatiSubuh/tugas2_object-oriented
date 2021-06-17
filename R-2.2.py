09b7920549482241ad0213fdaafd5a660025869e
NurRahmawatiSubuh <58314157+NurRahmawatiSubuh@users.noreply.github.com>
2021-06-16 18:51:42 -1200

    Add files via upload

---
+++ R-2.2.py
@@ -0,0 +1,6 @@
+# Aplikasi yang mengikuti perkembangan zaman
+# Tiktok
+# salah satu media sosial yang sedang menjadi trend saat ini adalah tiktok.
+# Tiktok kini seakan menjadi aplikasi primadona yang layak  dan wajib diunduh oleh semua orang.
+# Fitur baru tiktok merupakan hal yang patut kamu ketahui sebagai pengguna aktif sosial media tersebut.
+# perkembangan fitur ini tentu akan berdampak pada cara pemakaian aplikasi yang terus digunakan secara berkala oleh para penikmatnya.
